29bfd954307bb6169c4969a55cd985c7d755cc74
nelsonle345 <nelsonle345@gmail.com>
2023-06-25 10:18:17 -0400

    Add the database to the link

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/books229"
-  "URL": "mongodb+srv://nelsonle345:4lbPBcSzx6Y6h7Ap@mongodbserver-midterm.dh0n9ni.mongodb.net/?retryWrites=true&w=majority"
+  "URL": "mongodb+srv://nelsonle345:4lbPBcSzx6Y6h7Ap@mongodbserver-midterm.dh0n9ni.mongodb.net/book_store?retryWrites=true&w=majority"
 };
